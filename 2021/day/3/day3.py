28e3fc704d8eee4e9738f578daee8366c4dcaef8
laith <lhasanian@gmail.com>
2021-12-16 12:38:08 -0800

    day3

--- 2021/day/3/day3.py
+++ 2021/day/3/day3.py
@@ -1,24 +1,42 @@
-def part1():
-    inputFile = open('/Users/laith/adventOfCode/2021/day3/test.txt')
-    grid = []
-    for line in inputFile:
-        row = []
-        for char in line.strip():
-            row.append(int(char))
-        print(row)
+class Diagnostic:
+    def __init__(self, inputFile):
+        self.grid = []
+        self.columnalSum = []
+        for line in inputFile:
+            row = []
+            for char in line.strip():
+                row.append(int(char))
+            self.grid.append(row)
+        self.rows = len(self.grid)
+        self.cols = len(self.grid[0])
 
-    print('part 1: ' + str(1))
+    def printReport(self):
+        for row in self.grid:
+            print(row)
+
+    def printColumnalSum(self):
+        for i in range(self.cols):
+            self.columnalSum.append(0)
+        for row in self.grid:
+            for i in range(self.cols):
+                self.columnalSum[i] += row[i]
+        print("columnalSum:")
+        print(self.columnalSum)
+        print('rows:')
+        print(self.rows)
+
+def part1():
+    inputFile = open('/Users/laith/adventOfCode/2021/day/3/test.txt')
+    report = Diagnostic(inputFile)
+    print('part 1:')
+    report.printReport()
+    report.printColumnalSum()
 
 def part2():
-    inputFile = open('/Users/laith/adventOfCode/2021/day3/test.txt')
+    inputFile = open('/Users/laith/adventOfCode/2021/day/3/test.txt')
     for line in inputFile:
         pass
-    print('part 2: ' + str(2))
-
-def columnalSum(grid):
-    for row in grid:
-        for value in row:
-            pass
+    print('\npart 2: ' + str(2))
 
 
 
