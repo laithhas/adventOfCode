ddfb25277bf4e7f804546c63ae9cc7ad02867ecc
Laith Hasanian <laith@moistmbp-eir.local>
2022-09-05 10:48:38 -0500

    Starting day 5

---
+++ 2021/day/5/solution.py
@@ -0,0 +1,23 @@
+# def part1():
+#     inputFile = open('/Users/laith/adventOfCode/2021/day/x/test.txt')
+#     inputFile.close()
+
+
+# def part2():
+#     inputFile = open('/Users/laith/adventOfCode/2021/day/x/test.txt')    
+#     inputFile.close()
+
+# part1()
+# part2()
+
+def part1():
+    inputFile = open('/Users/laith/adventOfCode/2021/day/5/test.txt')
+    inputFile.close()
+
+
+def part2():
+    inputFile = open('/Users/laith/adventOfCode/2021/day/5/test.txt')    
+    inputFile.close()
+
+part1()
+part2()
